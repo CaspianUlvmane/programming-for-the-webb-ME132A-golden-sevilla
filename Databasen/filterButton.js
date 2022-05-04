3d35d1323e4c0f1d7c9d8f683301fd6b23c2712a
Caspian Ulvmåne <cabbeulv@gmail.com>
2022-05-04 18:12:28 +0200

    filterButtons render and almost works

--- Databasen/filterButton.js
+++ Databasen/filterButton.js
@@ -28,16 +28,50 @@ function buildFilterButton (text, key, value){
 }
     
     function languageButtons(){
-        let body = document.querySelector("body")
+        let languages = document.getElementById("languages")
         for (let language of DB.LANGUAGES){
-            body.appendChild(buildFilterButton(language.name, "language", language.id))
+            languages.appendChild(buildFilterButton(language.name, "language", language.id))
         }
     }
 
-    function languageButtons(){
-        let body = document.querySelector("body")
-        for (let language of DB.LANGUAGES){
-            body.appendChild(buildFilterButton(language.name, "language", language.id))
+    function fieldButtons(){
+        let fields = document.getElementById("fields")
+        for (let field of DB.FIELDS){
+            fields.appendChild(buildFilterButton(field.name, "subjectID", field.id))
+        }
+    }
+
+    function levelButtons(){
+        let levels = document.getElementById("languages")
+        let i = 0
+        for (let level of DB.LEVELS){
+            levels.appendChild(buildFilterButton(level, "level", i))
+            i++
+        }
+    }
+
+    function countryButtons(){
+        let countries = document.getElementById("countries")
+        for (let country of DB.COUNTRIES){
+            let countryButton = buildFilterButton(country.name, "country", country.id)
+            countryButton.addEventListener("click", function (){cityButtons()})
+            countries.appendChild(countryButton)
+        }
+        console.log(countries)
+        return countries.join(" ")
+    }
+
+    function cityButtons(){
+        let citiesDiv = document.getElementById("cities")
+        citiesDiv.innerHTML = " "
+        let active = document.querySelectorAll(".active")
+        for (let country of active){
+        let data = JSON.parse(country.dataset.data)
+            if (data.key == "country")
+                for (let city of DB.CITIES){
+                    if (city.countryID == data.value)
+                    citiesDiv.appendChild(buildFilterButton(city.name, "city", city.id))
+                }
         }
     }
     
@@ -46,4 +80,24 @@ function buildFilterButton (text, key, value){
             this.classList.remove("active")
         }
         else this.classList.add("active")
+    }
+
+    function buildFilterButtons (){
+        let body = document.querySelector("body")
+        let div = document.createElement("div")
+        div.innerHTML = `<div>Språk</div>
+        <div id="languages"></div>
+        <div>Nivåer</div>
+        <div id="levels"></div>
+        <div>Ämnen</div>
+        <div id="fields"></div>
+        <div>Länder</div>
+        <div id="countries"></div>
+        <div>Städer</div>
+        <div id="cities"></div>`
+        body.appendChild(div)
+        languageButtons()
+        levelButtons()
+        fieldButtons()
+        countryButtons()
     }
