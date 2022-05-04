0bc175ca566957dcaae660cf633cdc7508a3b38c
sophiesundqvist <sophie.sundqvist@hotmail.com>
2022-05-04 19:37:26 +0200

    added eventListener to change class from active-view
    still need to add code to change which container, map or list container that will shows when clickling

--- continent_list_code/continent_list.js
+++ continent_list_code/continent_list.js
@@ -1,7 +1,7 @@
 
 function createContinentList(){
     let listContainer = document.createElement("div")
-    listContainer.id = "list-container"
+    listContainer.classList.add("list-container")
 
     for (let continent of DB.CONTINENTS){
         let continetContainer = document.createElement("div")
@@ -32,9 +32,6 @@ function createContinentList(){
     return listContainer
 }
 
-cotinentViewBar()
-let list = createContinentList()
-document.querySelector("body").appendChild(list)
 
 
 function getClickedCountryId(event){
@@ -46,9 +43,18 @@ function getClickedCountryId(event){
 
 function cotinentViewBar (){
     let viewContainer = document.createElement("div")
-    viewContainer.appendChild(createDivMapView())
-    viewContainer.appendChild(createDivListView())
-
+    viewContainer.id = "view-container"
+    let mapDiv= createDivMapView()
+    let listDiv = createDivListView()
+    mapDiv.addEventListener("click", function(){
+        changeToMapView(listDiv, mapDiv)
+    })
+    listDiv.addEventListener("click", function(){
+        changeToListView(listDiv,mapDiv)
+    })
+    viewContainer.appendChild(mapDiv)
+    viewContainer.appendChild(listDiv)
+    
     document.querySelector("body").appendChild(viewContainer)
 }
 
@@ -56,7 +62,7 @@ function createDivMapView (){
     let mapDiv = document.createElement("div")
     mapDiv.classList.add("map-view")
     mapDiv.innerHTML = "Kartvy"
-
+    
     return mapDiv
 }
 
@@ -64,6 +70,21 @@ function createDivListView (){
     let listDiv = document.createElement("div")
     listDiv.classList.add("list-view")
     listDiv.innerHTML = "Listvy"
-
+    
     return listDiv
 }
+
+// need to add code to change beetween continentListContainer and continerntMapContainer
+function changeToListView(listDiv, mapDiv){    
+    listDiv.classList.add("active-view")
+    mapDiv.classList.remove("active-view")
+}
+
+function changeToMapView(listDiv, mapDiv){  
+    listDiv.classList.remove("active-view")
+    mapDiv.classList.add("active-view")
+}
+
+cotinentViewBar()
+let list = createContinentList()
+document.querySelector("body").appendChild(list)
