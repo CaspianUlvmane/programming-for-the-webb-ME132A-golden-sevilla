52473296511d17778e6658257586dd38deea465c
tbjorklind <97900554+tbjorklind@users.noreply.github.com>
2022-05-05 11:41:41 +0200

    Made burger-array

---
+++ pseudo-code/burger_items.js
@@ -0,0 +1,22 @@
+function createBurgerItems () {
+  let burgerArray = [
+    {
+      text: 'Utbyteslivet',
+      url: 'info.html'
+    },
+    {
+      text: 'Världsdelar och länder',
+      url: 'continets.html'
+    },
+    {
+      text: 'Ämnen',
+      url: 'fields.html'
+    },
+    {
+      text: 'Om oss',
+      url: 'about.html'
+    }
+  ]
+
+  let burgerContainer = document.createElement('div')
+  }
