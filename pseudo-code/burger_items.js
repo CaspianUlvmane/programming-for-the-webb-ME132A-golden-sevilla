aadbd980f0a74b5a6d2181c92c0dd7b57ba29c84
tbjorklind <97900554+tbjorklind@users.noreply.github.com>
2022-05-05 13:06:14 +0200

    Created burger-items and added links

--- pseudo-code/burger_items.js
+++ pseudo-code/burger_items.js
@@ -1,5 +1,7 @@
+document.querySelector('body').appendChild(createBurgerItems())
+
 function createBurgerItems () {
-  let burgerArray = [
+  let burgerItems = [
     {
       text: 'Utbyteslivet',
       url: 'info.html'
@@ -13,10 +15,20 @@ function createBurgerItems () {
       url: 'fields.html'
     },
     {
+      text: 'Program',
+      url: 'filter.html'
+    },
+    {
       text: 'Om oss',
       url: 'about.html'
     }
   ]
-
+  console.log(burgerItems)
   let burgerContainer = document.createElement('div')
+  for (let item of burgerItems) {
+    let burgerItem = document.createElement('div')
+    burgerItem.innerHTML = `<a href = "${item.url}">${item.text}</a> `
+    burgerContainer.appendChild(burgerItem)
   }
+  return burgerContainer
+}
