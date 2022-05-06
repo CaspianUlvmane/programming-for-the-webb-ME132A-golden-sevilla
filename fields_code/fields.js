62068661f976adcd527e4eddc1dd86a45ee9f512
tbjorklind <97900554+tbjorklind@users.noreply.github.com>
2022-05-06 08:19:37 +0200

    Added img, button and style to function

--- fields_code/fields.js
+++ fields_code/fields.js
@@ -1,10 +1,16 @@
 function createFieldView () {
   let fieldContainer = document.createElement('div')
   document.querySelector('body').appendChild(fieldContainer)
+  fieldContainer.classList.add('fields-container')
 
   for (let field of DB.FIELDS) {
     let fieldDiv = document.createElement('div')
-    fieldDiv.innerHTML = `${field.name} + ${field.text}`
+    fieldDiv.classList.add('field-div')
+    fieldDiv.innerHTML = `
+    <div class="field-img"></div>
+    <h3>${field.name}</h3>
+    <p>${field.text}</p>
+    <button class="field-button">${'Se alla program'}</button>`
     fieldContainer.appendChild(fieldDiv)
   }
 }
