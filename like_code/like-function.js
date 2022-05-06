9a0e080ab7f2296cfb34915e877c2e93faab5622
tbjorklind <97900554+tbjorklind@users.noreply.github.com>
2022-05-06 15:29:56 +0200

    Added code to function

--- like_code/like-function.js
+++ like_code/like-function.js
@@ -1,4 +1,4 @@
-let array = ['hej', 'hej']
+let array = [3, 2]
 
 function likedPrograms (likedArray) {
   let likeContainer = document.createElement('div')
@@ -7,11 +7,16 @@ function likedPrograms (likedArray) {
 
   if (likedArray.length > 0) {
     for (let liked of likedArray) {
+      // om array består av namn, ändra från id till namn.
+      let program = DB.PROGRAMMES.find(program => program.id == liked)
+      console.log(program)
       let likedItem = document.createElement('div')
-      likedItem.innerHTML = `${liked}`
+      // lägg till resten av infon.
+      likedItem.innerHTML = `${program.name}`
       likeContainer.appendChild(likedItem)
     }
   } else {
+// div, med innerhtml, text. append to container.
     console.log('finns inga liked programs')
   }
 }
@@ -24,4 +29,8 @@ function changeClassOnLikeContainer (element) {
   } else {
     element.classList.add('container-show')
   }
+}
+
+function findLikedProgram () {
+
 }
