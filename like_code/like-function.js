cfc5a956746408b654df83799f9728e9a82224f3
tbjorklind <97900554+tbjorklind@users.noreply.github.com>
2022-05-06 12:03:03 +0200

    New function to change class on like-container

--- like_code/like-function.js
+++ like_code/like-function.js
@@ -1,19 +1,27 @@
-let array = ["hej", "hej"]
+let array = ['hej', 'hej']
 
 function likedPrograms (likedArray) {
-    let likeContainer = document.createElement("div")
-    likeContainer.classList.add("container-hidden")
-    document.querySelector("body").appendChild(likeContainer)
-    
-    if (likedArray.length > 0){
-        for (let liked of likedArray){
-            let likedItem = document.createElement("div")
-            likedItem.innerHTML = `${liked}`
-            likeContainer.appendChild(likedItem)
-        }
-    } else {
-        console.log("finns inga liked")
+  let likeContainer = document.createElement('div')
+  likeContainer.classList.add('container-hidden')
+  document.querySelector('body').appendChild(likeContainer)
+
+  if (likedArray.length > 0) {
+    for (let liked of likedArray) {
+      let likedItem = document.createElement('div')
+      likedItem.innerHTML = `${liked}`
+      likeContainer.appendChild(likedItem)
     }
+  } else {
+    console.log('finns inga liked programs')
+  }
 }
 
 likedPrograms(array)
+
+function changeClassOnLikeContainer (element) {
+  if (element.classList.contains('container-show')) {
+    element.classList.remove('container-show')
+  } else {
+    element.classList.add('container-show')
+  }
+}
