387342be86db30b638005afad6eef71433ed4081
LiamHjalmarsson <Liam.hjalmarsson@hotmail.com>
2022-05-13 12:37:28 +0200

    s

--- js_files/hompage.js
+++ js_files/hompage.js
@@ -5,10 +5,3 @@ buildTopMenu()
 // document.querySelector('header').appendChild(buildTopMenu())
 // document.querySelector('header').appendChild(createBurgerItems())
 likedPrograms()
-
-
-let downButton = document.querySelector("#scroll-down");
-
-downButton.addEventListener("click", function () {
-    document.querySelector("#text-info").scrollIntoView({behavior: "smooth"})
-})
