b3ff5284854d47d7ff3b8e96357acbb5d1a6428b
LiamHjalmarsson <Liam.hjalmarsson@hotmail.com>
2022-05-13 11:50:07 +0200

    home page some more styling and down function

--- js_files/hompage.js
+++ js_files/hompage.js
@@ -5,3 +5,10 @@ buildTopMenu()
 // document.querySelector('header').appendChild(buildTopMenu())
 // document.querySelector('header').appendChild(createBurgerItems())
 likedPrograms()
+
+
+let downButton = document.querySelector("#scroll-down");
+
+downButton.addEventListener("click", function () {
+    document.querySelector("#text-info").scrollIntoView({behavior: "smooth"})
+})
